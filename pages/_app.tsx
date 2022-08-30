6a829e96fbe464c631a3b1f17861f6b0b2fa91f0
malikaindeewari <malika@rooster.org>
2022-08-30 21:52:12 +0530

    tag manager code added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import 'antd/dist/antd.css'
 import '../styles/vars.css'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" />
+        <link rel="manifest" href="/manifest.json" />
+
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
